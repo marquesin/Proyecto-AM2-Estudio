7b2f2aa394ab7434f30cae34e4f4f27942bdba47
marquesin <marquesinro@gmail.com>
2021-09-10 18:01:05 -0300

    Que hacemos sin interacciones

--- src/Home/QueHacemos/QueHacemos.js
+++ src/Home/QueHacemos/QueHacemos.js
@@ -3,17 +3,29 @@ import "./QueHacemos.css";
 import icon1 from "./Img/Icons/Icon 1.svg";
 import icon2 from "./Img/Icons/Icon 2.svg";
 import icon3 from "./Img/Icons/Icon 3.svg";
+import ServiciosDesplegados from "./Servicios/ServiciosDesplegados";
 
 export default function QueHacemos() {
   return (
-    <section>
-      <h2 className="titleDeSectiones">QUÉ HACEMOS</h2>
-      <div>
-        iconos presionables y servicios desplegados
-        <img src={icon1} alt="" className="iconServicios" />
-        <img src={icon2} alt="" />
-        <img src={icon3} alt="" />
+    <section className="containerQH">
+      <h2 className="titleDeSectiones QH">QUÉ HACEMOS</h2>
+      <div className="containerButtonQH">
+        <div>
+          <img src={icon1} alt="" />
+          <h1 className="title-Icon-QH">MARKETING</h1>
+        </div>
+        <div className="linea" />
+        <div>
+          <img src={icon2} alt="" />
+          <h1 className="title-Icon-QH">DISEÑO</h1>
+        </div>
+        <div className="linea" />
+        <div>
+          <img src={icon3} alt="" />
+          <h1 className="title-Icon-QH">PRINT</h1>
+        </div>
       </div>
+      <ServiciosDesplegados />
       <button>NUESTRO BOOK</button>
     </section>
   );
---
+++ src/Home/QueHacemos/Servicios/ServiciosDesplegados.js
@@ -0,0 +1,63 @@
+import React from "react";
+import "./ServiciosDesplegados.css";
+
+export default function ServiciosDesplegados() {
+  return (
+    <section className="containerServiciosDesplegados">
+      <div className="item-ServicioDesplegado">
+        <div className="containerFotosServiciosDesplegados">
+          <div>
+            <div className="foto1SD">Foto1</div>
+            <div className="foto2SD">Foto2</div>
+          </div>
+          <div>
+            <div className="foto3SD">Foto3</div>
+            <div className="foto4SD">Foto4</div>
+          </div>
+        </div>
+        <div className="containerTxtServiciosDesplegados">
+          <h2 className="titleSD">MARKETING</h2>
+          <p className="pSD">
+            Si tienes una idea, proyecto o reto, con gusto lo revisamos juntos.
+          </p>
+        </div>
+      </div>
+      <div className="item-ServicioDesplegado">
+        <div className="containerFotosServiciosDesplegados">
+          <div>
+            <div className="foto1SD">Foto1</div>
+            <div className="foto2SD">Foto2</div>
+          </div>
+          <div>
+            <div className="foto3SD">Foto3</div>
+            <div className="foto4SD">Foto4</div>
+          </div>
+        </div>
+        <div className="containerTxtServiciosDesplegados">
+          <h2 className="titleSD">DISEÑO</h2>
+          <p className="pSD">
+            Si tienes una idea, proyecto o reto, con gusto lo revisamos juntos.
+          </p>
+        </div>
+      </div>
+      <div className="item-ServicioDesplegado">
+        <div className="containerFotosServiciosDesplegados">
+          <div>
+            <div className="foto1SD">Foto1</div>
+            <div className="foto2SD">Foto2</div>
+          </div>
+          <div>
+            <div className="foto3SD">Foto3</div>
+            <div className="foto4SD">Foto4</div>
+          </div>
+        </div>
+        <div className="containerTxtServiciosDesplegados">
+          <h2 className="titleSD">PRINT</h2>
+          <p className="pSD">
+            Si tienes una idea, proyecto o reto, con gusto lo revisamos juntos.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
